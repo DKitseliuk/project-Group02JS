d4246569992ca2a518d9f43788452a5708bad5d9
yarik <aroslavpavlivskij@gmail.com>
2025-11-08 17:33:51 +0200

    fix styles burger

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -2,25 +2,16 @@ export function initBurgerMenu() {
   const burgerBtn = document.querySelector('.header-burger-btn');
   const closeBtn = document.querySelector('.burger-menu-close');
   const burgerMenu = document.querySelector('.burger-menu');
-
+  const anchorLinks = burgerMenu.querySelectorAll('a[href^="#"]');
   burgerBtn.addEventListener('click', () => {
     burgerMenu.classList.add('is-open');
     document.body.classList.add('no-scroll');
   });
 
-  closeBtn.addEventListener('click', () => {
-    closeBurgerMenu();
-  });
+  closeBtn.addEventListener('click', closeBurgerMenu);
 
-  document.addEventListener('click', event => {
-    const isClickInsideMenu = burgerMenu.contains(event.target);
-    const isClickOnButton = burgerBtn.contains(event.target);
-
-    if (
-      burgerMenu.classList.contains('is-open') &&
-      !isClickInsideMenu &&
-      !isClickOnButton
-    ) {
+  burgerMenu.addEventListener('click', event => {
+    if (event.target === burgerMenu) {
       closeBurgerMenu();
     }
   });
@@ -29,6 +20,20 @@ export function initBurgerMenu() {
     if (event.key === 'Escape' && burgerMenu.classList.contains('is-open')) {
       closeBurgerMenu();
     }
+  });
+
+  anchorLinks.forEach(link => {
+    link.addEventListener('click', event => {
+      event.preventDefault();
+      const targetId = link.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        closeBurgerMenu();
+        setTimeout(() => {
+          targetElement.scrollIntoView({ behavior: 'smooth' });
+        }, 50);
+      }
+    });
   });
 
   function closeBurgerMenu() {
