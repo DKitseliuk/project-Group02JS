230455675d5b32bb35b4c4decaad0fbb1739563a
Bas <basarab.o@icloud.com>
2025-11-10 17:59:26 +0200

    add raty

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
-
 import Swiper from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Navigation, Pagination, Keyboard, Mousewheel } from 'swiper/modules';
+import Raty from 'raty-js';
+import star from '../img/star.svg';
+import starOn from '../img/staron.svg';
+import starHalf from '../img/starhalf.svg';
 
 const reviewsUrl = 'https://furniture-store-v2.b.goit.study/api/feedbacks';
 const list = document.querySelector('.swiper-wrapper');
@@ -28,7 +31,7 @@ function renderFeedbacks(reviews) {
       return `
         <li class="swiper-slide">
         <div class="feedback-item">
-          <p class="feedback-rate"> ${rate}</p>
+          <div class="feedback-rate try-raty" data-score="${rate}"></div>
           <p class="feedback-descr">${descr}</p>
           <p class="feedback-author">${name}</p>
           </div>
@@ -37,6 +40,20 @@ function renderFeedbacks(reviews) {
     .join('');
 
   list.innerHTML = markup;
+
+  const tryRaty = document.querySelectorAll('.try-raty').forEach(tryRaty => {
+    const raty = new Raty(tryRaty, {
+      number: 5,
+      rating: '${rate}',
+      step: 0.5,
+      readOnly: true,
+      starOff: star,
+      starOn: starOn,
+      starHalf: starHalf,
+      round: { down: 0.29, full: 0.5, up: 0.8 },
+    });
+    raty.init();
+  });
 }
 
 export { getFeedbacks };
@@ -81,6 +98,7 @@ function initSwiper() {
 getFeedbacks().then(feedbacks => {
   if (feedbacks) {
     renderFeedbacks(feedbacks);
+
     initSwiper();
   }
 });
